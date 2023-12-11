d9d9a7e1f288882d25054b36de9bd2dc711717ae
bumb11ebee <bumb11ebee@icloud.com>
2023-12-10 22:27:01 -0700

    Spaced the ticks better

--- app.py
+++ app.py
@@ -9,17 +9,23 @@ import base64
 
 app = Flask(__name__)
 
-def plot_coordinates(drawing):
+def plot_coordinates(drawing,color=None):
     fig, ax = plt.subplots()
     
     for section, coordinates in drawing.items():
         x_values, y_values = zip(*coordinates)
-        ax.plot(x_values, y_values, marker='o', label=section)
+        if color == None:
+            ax.plot(x_values, y_values, marker='o', label=section)
+        else:
+            ax.plot(x_values, y_values, marker='o', label=section,color=color)
 
-    ax.legend()
+    #ax.legend()
     ax.set_xlabel('X-axis')
     ax.set_ylabel('Y-axis')
-
+    ax.grid(True, which='both', linestyle='--', linewidth=1, alpha=0.5)  # Customize grid lines# Set ticks at each 1 unit for both x and y axes
+    ax.set_xticks(range(int(min(ax.get_xlim())), int(max(ax.get_xlim())) + 1, 1))
+    ax.set_yticks(range(int(min(ax.get_ylim())), int(max(ax.get_ylim())) + 1, 1))
+    fig.tight_layout()
     canvas = FigureCanvas(fig)
     img = BytesIO()
     fig.savefig(img, format='png')
@@ -36,6 +42,15 @@ def index():
         drawing = json.load(file)
 
     img_base64 = plot_coordinates(drawing)
+    
+    return render_template('index.html', img_base64=img_base64)
+
+@app.route('/black')
+def index2():
+    with open('drawing.json', 'r') as file:
+        drawing = json.load(file)
+
+    img_base64 = plot_coordinates(drawing,"black")
     
     return render_template('index.html', img_base64=img_base64)
 
