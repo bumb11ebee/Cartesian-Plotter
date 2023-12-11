dbf8cc54919c6081a4959a68e848858838294d6e
bumb11ebee <bumb11ebee@icloud.com>
2023-12-10 22:51:50 -0700

    Explained my code

--- app.py
+++ app.py
@@ -10,34 +10,42 @@ import base64
 app = Flask(__name__)
 
 def plot_coordinates(drawing,color=None):
-    fig, ax = plt.subplots()
+    """
+    Plot coordinates from a drawing dictionary
+    - drawing (dict): A dictionary containing the segments as keys and the Cartesian Coordinates as a list
+    Returns:
+    - str: Base64-encoded image of the plot
+    """
+
+    fig, ax = plt.subplots() #Creates the space where my plots will go
     
-    for section, coordinates in drawing.items():
-        x_values, y_values = zip(*coordinates)
+    for section, coordinates in drawing.items(): #Loops through the dictionary for each segment
+        x_values, y_values = zip(*coordinates) #Loads all of the coordinates into a list of x and a list of y
         if color == None:
-            ax.plot(x_values, y_values, marker='o', label=section)
+            ax.plot(x_values, y_values, marker='o', label=section) #draws all segments in their own color
         else:
-            ax.plot(x_values, y_values, marker='o', label=section,color=color)
+            ax.plot(x_values, y_values, marker='o', label=section,color=color) #draws all segments in black
 
     #ax.legend()
     ax.set_xlabel('X-axis')
     ax.set_ylabel('Y-axis')
     ax.grid(True, which='both', linestyle='--', linewidth=1, alpha=0.5)  # Customize grid lines# Set ticks at each 1 unit for both x and y axes
-    ax.set_xticks(range(int(min(ax.get_xlim())), int(max(ax.get_xlim())) + 1, 1))
-    ax.set_yticks(range(int(min(ax.get_ylim())), int(max(ax.get_ylim())) + 1, 1))
-    fig.tight_layout()
-    canvas = FigureCanvas(fig)
-    img = BytesIO()
-    fig.savefig(img, format='png')
-    img.seek(0)
+    ax.set_xticks(range(int(min(ax.get_xlim())), int(max(ax.get_xlim())) + 1, 1)) #Draws grid lines at every 1 unit
+    ax.set_yticks(range(int(min(ax.get_ylim())), int(max(ax.get_ylim())) + 1, 1)) #Draws grid lines at every 1 unit
+    fig.tight_layout() #Spaces the grid lines out so you can read them better
+    canvas = FigureCanvas(fig) #Updates the figure with all of my segments
+    img = BytesIO() #Creates a balnk image file
+    fig.savefig(img, format='png') #Writes my plots to the blank image
+    img.seek(0) #Sets the reading point of the file to the beginning
     
-    img_base64 = base64.b64encode(img.getvalue()).decode()
+    img_base64 = base64.b64encode(img.getvalue()).decode() #No idea about this but it rewrites the image into a format that the computer will draw
     
-    plt.close(fig)
+    plt.close(fig) #Deletes the Coordinates because they are now saved in the image
     return img_base64
 
 @app.route('/')
 def index():
+    #http://localhost:8080 will give you a multi coloured drawing
     with open('drawing.json', 'r') as file:
         drawing = json.load(file)
 
@@ -47,6 +55,7 @@ def index():
 
 @app.route('/black')
 def index2():
+    #http://localhost:8080/black will give you a black and white drawing
     with open('drawing.json', 'r') as file:
         drawing = json.load(file)
 
